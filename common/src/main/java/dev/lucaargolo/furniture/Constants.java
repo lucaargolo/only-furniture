f6d99ec5bee947e7cdbca49fe2386c0ca8fca87f
lucaargolo <lucaargolo@gmail.com>
2025-10-21 10:53:31 -0300

    Initial commit

---
+++ common/src/main/java/dev/lucaargolo/furniture/Constants.java
@@ -0,0 +1,11 @@
+package dev.lucaargolo.furniture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "furniture";
+    public static final String MOD_NAME = "Furniture Mod";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
