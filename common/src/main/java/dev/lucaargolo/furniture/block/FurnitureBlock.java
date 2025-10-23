5251b33192e469212fadb91826efda6bfc1d375b
lucaargolo <lucaargolo@gmail.com>
2025-10-23 10:39:23 -0300

    Voxel shapes

--- common/src/main/java/dev/lucaargolo/furniture/block/FurnitureBlock.java
+++ common/src/main/java/dev/lucaargolo/furniture/block/FurnitureBlock.java
@@ -2,16 +2,28 @@ package dev.lucaargolo.furniture.block;
 
 import dev.lucaargolo.furniture.data.FurnitureData;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 public class FurnitureBlock extends Block {
 
-    public FurnitureBlock(Block base) {
+    private final VoxelShape shape;
+
+    public FurnitureBlock(Block base, VoxelShape... shapes) {
         super(BlockBehaviour.Properties.ofFullCopy(base).noOcclusion());
+        VoxelShape shape = Shapes.empty();
+        for (VoxelShape s : shapes) {
+            shape = Shapes.join(shape, s, BooleanOp.OR);
+        }
+        this.shape = shape;
     }
 
     @Override
@@ -20,4 +32,13 @@ public class FurnitureBlock extends Block {
         FurnitureData.set(pLevel, pPos, FurnitureData.DEFAULT);
     }
 
+    @Override
+    protected @NotNull VoxelShape getShape(@NotNull BlockState pState, @NotNull BlockGetter pLevel, @NotNull BlockPos pPos, @NotNull CollisionContext pContext) {
+        if(pLevel instanceof Level level) {
+            FurnitureData data = FurnitureData.get(level, pPos);
+            return this.shape.move(data.getX(), 0.0, data.getZ());
+        }else{
+            return this.shape;
+        }
+    }
 }
--- common/src/main/java/dev/lucaargolo/furniture/block/ModBlocks.java
+++ common/src/main/java/dev/lucaargolo/furniture/block/ModBlocks.java
@@ -5,6 +5,7 @@ import dev.lucaargolo.furniture.utils.ModRegistry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.phys.shapes.Shapes;
 
 import java.util.function.Supplier;
 
@@ -12,6 +13,14 @@ public class ModBlocks {
 
     public static final ModRegistry<Block> BLOCKS = FurnitureMod.INSTANCE.registry(Registries.BLOCK);
 
-    public static Supplier<FurnitureBlock> SMALL_TABLE = BLOCKS.register("small_table", () -> new FurnitureBlock(Blocks.OAK_PLANKS));
+    public static Supplier<FurnitureBlock> SMALL_TABLE = BLOCKS.register("small_table", () -> new FurnitureBlock(Blocks.OAK_PLANKS,
+        Shapes.box(0.125, 0, 0.75, 0.25, 0.625, 0.875),
+        Shapes.box(0.125, 0, 0.125, 0.25, 0.625, 0.25),
+        Shapes.box(0.75, 0, 0.125, 0.875, 0.625, 0.25),
+        Shapes.box(0.75, 0, 0.75, 0.875, 0.625, 0.875),
+        Shapes.box(0, 0.625, 0.125, 1, 0.75, 0.875),
+        Shapes.box(0.125, 0.625, 0, 0.875, 0.75, 0.125),
+        Shapes.box(0.125, 0.625, 0.875, 0.875, 0.75, 1)
+    ));
 
 }
